a94d0e23dc3aacfd735ca697ea41c364c4d9d68a
takenokoroid <takenokoroid@gmail.com>
2023-10-14 12:52:45 +0000

    Headerコンポーネントの作成

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let title = 'Default Title';
+</script>
+
+<!--
+@component
+ヘッダーコンポーネント
+- Usage:
+  ```tsx
+  <Headers title="Word Wolf">
+    ```
+  -->
+<header>
+	<h1>{title}</h1>
+</header>
+
+<style>
+	header {
+		text-align: center;
+		padding: 1rem;
+		border-bottom: 1px solid var(--secondary-light);
+	}
+</style>

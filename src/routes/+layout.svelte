0427a90f69884ac481755547c4de9cad5386e762
takenokoroid <takenokoroid@gmail.com>
2023-10-14 12:58:54 +0000

    レイアウトの作成

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from '../lib/components/Header.svelte';
+</script>
+
+<Header title="Word Wolf" />
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		/* 中央寄せ */
+		display: flex;
+		justify-content: center;
+		/*余白*/
+		padding: 1rem;
+	}
+</style>
